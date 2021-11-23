cbc255dfd54326ddedbdc631fbfbc30ec69c035f
LuigiToniolo <luigi.toniolo@domvsit.com.br>
2021-11-23 15:40:51 -0300

    Upando o curso de orientação de cursos

---
+++ src/Titular.php
@@ -0,0 +1,19 @@
+<?php
+
+class Titular extends Pessoa
+{
+    private $endereco;
+
+    public function __construct(CPF $cpf, string $nome, Endereco $endereco)
+    {
+        $this->cpf = $cpf;
+        $this->validaNomeTitular($nome);
+        $this->nome = $nome;
+        $this->endereco = $endereco;
+    }
+
+    public function recuperaEndereco(): Endereco
+    {
+        return $this->endereco;
+    }
+}
